ece4ebac3ac8346400a9bd15441ea330a4f4e43a
vineet7071 <vineet.sehrawat121@gmail.com>
2025-08-12 13:34:53 +0530

    fifth commit

---
+++ TerraformFiles/main.tf
@@ -0,0 +1,27 @@
+  provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+
+resource "aws_vpc" "MyVpc" {
+  cidr_block = "10.0.0.0/16"
+}
+resource "aws_subnet" "public_subnet" {
+
+  vpc_id     = aws_vpc.MyVpc.id
+  cidr_block = "10.0.1.0/24"
+  
+  }
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.MyVpc.id
+  cidr_block = "10.0.2.0/24"
+  }
+resource "aws_instance" "Instance1" {
+  ami           = "ami-0ec73a27d7764e6b3"
+  instance_type = "t2.micro"
+  key_name = "mywindowskey"
+  subnet_id = aws_subnet.public_subnet.id
+ 
+} 
